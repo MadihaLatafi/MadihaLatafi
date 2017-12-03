49f3025cbd44f8906ef5d6698145976fa5bab729
Madiha Latafi <latafimadiha@hotmail.fr>
2017-12-03 17:10:36 +0100

    test front-end

--- rails-mister-cocktail/config/routes.rb
+++ rails-mister-cocktail/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+mount Attachinary::Engine => "/attachinary"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [ :index, :show, :new, :create ] do
+    resources :doses, only: [ :create ]
+  end
+  root "cocktails#index"
+  resources :doses, only: [ :destroy ]
 end
